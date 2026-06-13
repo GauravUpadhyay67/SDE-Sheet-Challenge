e93f90ac9e51f800b5d043b8f1a98893dcdf9921
Gaurav Upadhyay <148449614+GauravUpadhyay67@users.noreply.github.com>
2026-06-13 18:00:21 +0530

    Add function to clone linked list with random pointers
    Implemented the copyRandomList function to clone a linked list with random pointers.

--- Day13/38_CloneLLWithRandom.cpp
+++ Day13/38_CloneLLWithRandom.cpp
@@ -4,9 +4,39 @@
 // Approach: Interleave clone nodes, set random pointers, separate lists
 // Time: O(n) | Space: O(1)
 
+
+
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        // write code here
+        if (!head) return NULL;
+        Node* temp = head;
+
+        while(temp){
+            Node* newNode = new Node(temp->val);
+            newNode->next = temp->next;
+            temp->next = newNode;
+            temp = temp->next->next;
+        }
+
+        temp = head;
+        while (temp) {
+            if (temp->random)
+                temp->next->random = temp->random->next;
+            temp = temp->next->next;
+        }
+
+        Node* dummyHead = new Node(-1);
+        Node* copy = dummyHead;
+        temp = head;
+
+        while (temp) {
+            copy->next = temp->next;
+            temp->next = temp->next->next;
+
+            copy = copy->next;
+            temp = temp->next;
+        }
+        return dummyHead->next;
     }
 };
