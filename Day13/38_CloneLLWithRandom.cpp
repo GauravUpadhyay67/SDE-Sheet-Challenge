e70ba5fa75e296e018b785579670c4818d4178e7
Gaurav Upadhyay <148449614+GauravUpadhyay67@users.noreply.github.com>
2026-06-13 13:12:22 +0530

    Implement clone linked list with random pointer
    Added a class Solution with a method to clone a linked list with random pointers.

---
+++ Day13/38_CloneLLWithRandom.cpp
@@ -0,0 +1,12 @@
+// Problem: Clone Linked List with Random and Next Pointer
+// Link: https://leetcode.com/problems/copy-list-with-random-pointer/
+// Difficulty: Hard
+// Approach: Interleave clone nodes, set random pointers, separate lists
+// Time: O(n) | Space: O(1)
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        // write code here
+    }
+};
