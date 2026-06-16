6384b17bf16756c28f5a41c0fe76f6989921c86f
Gaurav Upadhyay <148449614+GauravUpadhyay67@users.noreply.github.com>
2026-06-16 22:03:55 +0530

    Update 50_SubsetsII.cpp

--- Day16/50_SubsetsII.cpp
+++ Day16/50_SubsetsII.cpp
@@ -4,9 +4,28 @@
 // Approach: Sort + Recursion, skip duplicates at same level
 // Time: O(2^n) | Space: O(2^n)
 
+
+
 class Solution {
 public:
+    void solve(vector<int>&nums, int index, vector<int>&temp, vector<vector<int>>&ans){
+        ans.push_back(temp);
+        for(int i=index; i<nums.size(); i++){
+            if(i > index && nums[i] == nums[i-1]) continue;
+
+            temp.push_back(nums[i]);
+            solve(nums, i+1, temp, ans);
+            temp.pop_back();
+        }
+    }
+
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        // paste your code here
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<int>temp;
+        vector<vector<int>>ans;
+
+        solve(nums, 0, temp, ans);
+        return ans;
     }
 };
