23ebe2725d3d3b3d334b9d30a9f81849828a2bfc
Gaurav Upadhyay <148449614+GauravUpadhyay67@users.noreply.github.com>
2026-06-16 21:15:10 +0530

    Update 49_SubsetSums.cpp

--- Day16/49_SubsetSums.cpp
+++ Day16/49_SubsetSums.cpp
@@ -4,6 +4,24 @@
 // Approach: Recursion — include/exclude each element, collect all sums
 // Time: O(2^n) | Space: O(2^n)
 
-void solve(vector<int>& arr, int i, int sum, vector<int>& result) {
-    // paste your code here
-}
+
+class Solution {
+    void solve(vector<int>&arr, int i, int sum, vector<int>&ans){
+        if(i < 0){
+            ans.push_back(sum);
+            return;
+        }
+        
+        solve(arr, i-1, sum+arr[i], ans);
+        solve(arr, i-1, sum, ans);
+    }
+  public:
+    vector<int> subsetSums(vector<int>& arr) {
+        int n = arr.size();
+        
+        vector<int>ans;
+        int sum = 0;
+        solve(arr, n-1, sum, ans);
+        return ans;
+    }
+};
