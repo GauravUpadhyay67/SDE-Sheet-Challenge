57304072af283b3e35e96499d623b8e308d47c6b
Gaurav Upadhyay <148449614+GauravUpadhyay67@users.noreply.github.com>
2026-06-21 17:49:50 +0530

    Update 65_MedianOf2SortedArrays.cpp

--- Day21/65_MedianOf2SortedArrays.cpp
+++ Day21/65_MedianOf2SortedArrays.cpp
@@ -1,5 +1,33 @@
 // Problem: Median of 2 Sorted Arrays
 // Link: https://leetcode.com/problems/median-of-two-sorted-arrays/
 // Difficulty: Hard
-// Approach: Binary Search on smaller array, partition both arrays
-// Time: O(log(min(n,m))) | Space: O(1)
+// Approach: Merge both sorted arrays, find median from merged array
+// Time: O(n+m) | Space: O(n+m)
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size();
+        int i = 0, j = 0;
+        vector<int>temp;
+
+        while(i < m && j < n){
+            if(nums1[i] < nums2[j]) temp.push_back(nums1[i++]);
+            else temp.push_back(nums2[j++]);
+        }
+
+        while(i < m) temp.push_back(nums1[i++]);
+        while(j < n) temp.push_back(nums2[j++]);
+
+        int newSize = m+n;
+        double ans = 0;
+        if(newSize&1){
+            ans = temp[newSize/2];
+        }
+        else{
+            ans = (temp[newSize/2] + temp[(newSize/2)-1])/2.0;
+        }
+        return ans;
+
+    }
+};
