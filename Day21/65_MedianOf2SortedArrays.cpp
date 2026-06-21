a6f6e49587260243e22b43e32a214d6768cbd16d
Gaurav Upadhyay <148449614+GauravUpadhyay67@users.noreply.github.com>
2026-06-21 17:02:31 +0530

    Create 65_MedianOf2SortedArrays.cpp

---
+++ Day21/65_MedianOf2SortedArrays.cpp
@@ -0,0 +1,5 @@
+// Problem: Median of 2 Sorted Arrays
+// Link: https://leetcode.com/problems/median-of-two-sorted-arrays/
+// Difficulty: Hard
+// Approach: Binary Search on smaller array, partition both arrays
+// Time: O(log(min(n,m))) | Space: O(1)
