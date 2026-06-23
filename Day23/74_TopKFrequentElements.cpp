04c6bfce8c1655972aaf301e9d855ea292775b71
Gaurav Upadhyay <148449614+GauravUpadhyay67@users.noreply.github.com>
2026-06-23 09:58:59 +0530

    Create 74_TopKFrequentElements.cpp

---
+++ Day23/74_TopKFrequentElements.cpp
@@ -0,0 +1,5 @@
+// Problem: Top K Frequent Elements
+// Link: https://leetcode.com/problems/top-k-frequent-elements/
+// Difficulty: Medium
+// Approach: HashMap for frequency + Min Heap of size k
+// Time: O(n log k) | Space: O(n)
