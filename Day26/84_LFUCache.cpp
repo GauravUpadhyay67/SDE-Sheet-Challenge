3c695abce0bb7b840d461856656e5a625c5464d0
Gaurav Upadhyay <148449614+GauravUpadhyay67@users.noreply.github.com>
2026-06-26 22:07:42 +0530

    Create 84_LFUCache.cpp

---
+++ Day26/84_LFUCache.cpp
@@ -0,0 +1,28 @@
+// Problem: LFU Cache
+// Link: https://leetcode.com/problems/lfu-cache/
+// Difficulty: Hard
+// Approach: HashMap + frequency map + DLL per frequency
+// Time: O(1) | Space: O(capacity)
+
+
+class LFUCache {
+public:
+    LFUCache(int capacity) {
+        
+    }
+    
+    int get(int key) {
+        
+    }
+    
+    void put(int key, int value) {
+        
+    }
+};
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache* obj = new LFUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
