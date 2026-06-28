51fd5988334ec5c67766492ef6810c0efb1467d6
Gaurav Upadhyay <148449614+GauravUpadhyay67@users.noreply.github.com>
2026-06-28 13:48:44 +0530

    Create 90_MaxOfMinimumsWindowSize.cpp

---
+++ Day28/90_MaxOfMinimumsWindowSize.cpp
@@ -0,0 +1,5 @@
+// Problem: Maximum of Minimums for Every Window Size
+// Link: https://www.geeksforgeeks.org/find-the-maximum-of-minimums-for-every-window-size-in-a-given-array/
+// Difficulty: Medium
+// Approach: Monotonic Stack — find left & right smaller, update answer array
+// Time: O(n) | Space: O(n)
