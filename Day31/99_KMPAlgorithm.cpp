47d6d4c18cebcd9999ddf8d46b914bf2d1c3530c
Gaurav Upadhyay <148449614+GauravUpadhyay67@users.noreply.github.com>
2026-07-01 21:32:57 +0530

    Create 99_KMPAlgorithm.cpp

---
+++ Day31/99_KMPAlgorithm.cpp
@@ -0,0 +1,5 @@
+// Problem: KMP Algorithm / LPS Array
+// Link: https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+// Difficulty: Hard
+// Approach: Build LPS array, use it to skip unnecessary comparisons
+// Time: O(n+m) | Space: O(m)
