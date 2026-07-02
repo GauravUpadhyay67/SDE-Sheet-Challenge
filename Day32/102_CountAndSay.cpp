b1b589f190aa8565428faceac085332b94730883
Gaurav Upadhyay <148449614+GauravUpadhyay67@users.noreply.github.com>
2026-07-02 21:45:36 +0530

    Update 102_CountAndSay.cpp

--- Day32/102_CountAndSay.cpp
+++ Day32/102_CountAndSay.cpp
@@ -3,3 +3,28 @@
 // Difficulty: Hard
 // Approach: Iteratively build each sequence by counting consecutive chars
 // Time: O(n × m) | Space: O(m)
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        string ans = "1";
+
+        for(int i=2; i<=n; i++){
+            string curr = "";
+            int cnt = 1;
+
+            for(int j=1; j<=ans.size(); j++){
+                if(j < ans.size() && ans[j] == ans[j-1]){
+                    cnt++;
+                }
+                else{
+                    curr += to_string(cnt);
+                    curr += ans[j-1];
+                    cnt = 1;
+                }
+            }
+            ans = curr;
+        }
+        return ans;
+    }
+};
