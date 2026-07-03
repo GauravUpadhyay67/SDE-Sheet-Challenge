37c5c30b441c4176d08d19e7f339139583285f0e
Gaurav Upadhyay <148449614+GauravUpadhyay67@users.noreply.github.com>
2026-07-03 15:22:16 +0530

    Create 106_PostorderTraversal.cpp

---
+++ Day33/106_PostorderTraversal.cpp
@@ -0,0 +1,5 @@
+// Problem: Postorder Traversal
+// Link: https://leetcode.com/problems/binary-tree-postorder-traversal/
+// Difficulty: Easy
+// Approach: Recursive — Left, Right, Root
+// Time: O(n) | Space: O(h)
