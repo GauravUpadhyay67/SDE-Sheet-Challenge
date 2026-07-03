ef73416e2efc613c172c549ac20a55a9dd928438
Gaurav Upadhyay <148449614+GauravUpadhyay67@users.noreply.github.com>
2026-07-03 22:44:58 +0530

    Update 107_MorrisInorderTraversal.cpp

--- Day33/107_MorrisInorderTraversal.cpp
+++ Day33/107_MorrisInorderTraversal.cpp
@@ -3,3 +3,48 @@
 // Difficulty: Hard
 // Approach: Morris — thread right pointer to inorder successor, O(1) space
 // Time: O(n) | Space: O(1)
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        TreeNode* currNode = root;
+
+        while(currNode){
+            if(!currNode->left){
+                ans.push_back(currNode->val);
+                currNode = currNode->right;
+            }
+            else{
+                TreeNode* tempNode = currNode->left;
+                while(tempNode->right && tempNode->right != currNode){
+                    tempNode = tempNode->right;
+                }
+
+                if(tempNode->right){
+                    tempNode->right = NULL;
+                    ans.push_back(currNode->val);
+                    currNode = currNode->right;
+                }
+                else{
+                    tempNode->right = currNode;
+                    currNode = currNode->left; 
+                }
+            }
+        }
+        return ans;
+    }
+};
