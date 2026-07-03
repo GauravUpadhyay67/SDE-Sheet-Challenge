586a15eb9ac6520b16c818a1ed4eba92e9572aeb
Gaurav Upadhyay <148449614+GauravUpadhyay67@users.noreply.github.com>
2026-07-03 15:22:03 +0530

    Create 105_PreorderTraversal.cpp

---
+++ Day33/105_PreorderTraversal.cpp
@@ -0,0 +1,5 @@
+// Problem: Preorder Traversal
+// Link: https://leetcode.com/problems/binary-tree-preorder-traversal/
+// Difficulty: Easy
+// Approach: Recursive — Root, Left, Right
+// Time: O(n) | Space: O(h)
