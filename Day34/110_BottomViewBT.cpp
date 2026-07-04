4e6c566647760bcabe6aab11f63f0721e5fb7527
Gaurav Upadhyay <148449614+GauravUpadhyay67@users.noreply.github.com>
2026-07-04 20:01:09 +0530

    Update 110_BottomViewBT.cpp

--- Day34/110_BottomViewBT.cpp
+++ Day34/110_BottomViewBT.cpp
@@ -3,3 +3,47 @@
 // Difficulty: Medium
 // Approach: BFS + HashMap — track horizontal distance, update with latest node
 // Time: O(n log n) | Space: O(n)
+
+
+
+/*
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x) {
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    vector<int> bottomView(Node *root) {
+        // code here
+        vector<int>ans;
+        map<int, Node*>mp;
+        queue<pair<int, Node*>>q;
+        
+        q.push({0, root});
+        
+        while(!q.empty()){
+            int col = q.front().first;
+            Node* node = q.front().second;
+            q.pop();
+            
+            if(node->left) q.push({col-1, node->left});
+            if(node->right) q.push({col+1, node->right});
+            
+            mp[col] = node;
+        }
+        
+        for(auto it: mp){
+            ans.push_back((it.second)->data);
+        }
+        return ans;
+    }
+};
