cf480cacc3b206912ec8a1a56b6d289d011a82da
Gaurav Upadhyay <148449614+GauravUpadhyay67@users.noreply.github.com>
2026-07-06 17:23:07 +0530

    Update 117_MaxDepthBT.cpp

--- Day36/117_MaxDepthBT.cpp
+++ Day36/117_MaxDepthBT.cpp
@@ -3,3 +3,27 @@
 // Difficulty: Medium
 // Approach: Recursion — max(left depth, right depth) + 1
 // Time: O(n) | Space: O(h)
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int solve(TreeNode* root){
+        if(!root) return 0;
+
+        return 1 + max(solve(root->left), solve(root->right));
+    }
+public:
+    int maxDepth(TreeNode* root) {
+        return solve(root);
+    }
+};
