0b3e7a8dc01c86d496de5c3c5aa041f29225fa77
Gaurav Upadhyay <148449614+GauravUpadhyay67@users.noreply.github.com>
2026-07-08 22:10:06 +0530

    Create 126_ConstructBTFromPostorderInorder.cpp

---
+++ Day38/126_ConstructBTFromPostorderInorder.cpp
@@ -0,0 +1,43 @@
+// Problem: Construct Binary Tree from Inorder and Postorder Traversal
+// Link: https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+// Difficulty: Medium
+// Approach: Recursively build the tree using postorder for the root and inorder to split left & right subtrees.
+// Time: O(n) | Space: O(n)
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* solve(vector<int>& inorder, int inStart, int inEnd, vector<int>& postorder, int postStart, int postEnd, map<int, int>&mp){
+        if(postStart > postEnd || inStart > inEnd) return NULL;
+
+        TreeNode* root = new TreeNode(postorder[postEnd]);
+        int inRoot = mp[postorder[postEnd]];
+        int numsLeft = inRoot - inStart;
+        
+        root->left = solve(inorder, inStart, inRoot - 1, postorder, postStart, postStart + numsLeft - 1, mp);
+        root->right = solve(inorder, inRoot + 1, inEnd, postorder, postStart + numsLeft, postEnd - 1, mp);
+
+        return root;
+    }
+
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        map<int, int>mp;
+        for(int i=0; i<inorder.size(); i++){
+            mp[inorder[i]] = i;
+        }
+
+        TreeNode* root = solve(inorder, 0, inorder.size()-1, postorder, 0, postorder.size()-1, mp);
+        return root;
+    }
+};
