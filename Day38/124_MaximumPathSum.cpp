b408d82cc69f88d75189623e9dac036f61026c8a
Gaurav Upadhyay <148449614+GauravUpadhyay67@users.noreply.github.com>
2026-07-08 21:41:06 +0530

    Create 124_MaximumPathSum.cpp

---
+++ Day38/124_MaximumPathSum.cpp
@@ -0,0 +1,36 @@
+// Problem: Binary Tree Maximum Path Sum
+// Link: https://leetcode.com/problems/binary-tree-maximum-path-sum/
+// Difficulty: Hard
+// Approach: DFS — calculate the maximum gain from each subtree while updating the global maximum path sum.
+// Time: O(n) | Space: O(h)
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int solve(TreeNode* root, int& ans){
+        if(!root) return 0;
+
+        int leftSum =  max(0, solve(root->left, ans));
+        int rightSum = max(0, solve(root->right,ans));
+        ans = max(ans, root->val + leftSum + rightSum);
+
+        return root->val + max(leftSum, rightSum);
+    }
+public:
+    int maxPathSum(TreeNode* root) {
+        int ans = INT_MIN;
+        solve(root, ans);
+        return ans;
+    }
+};
