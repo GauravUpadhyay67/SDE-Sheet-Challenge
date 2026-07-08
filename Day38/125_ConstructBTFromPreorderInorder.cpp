954d84dfcd369df7fecd16ebac28c2c6d5ac6e53
Gaurav Upadhyay <148449614+GauravUpadhyay67@users.noreply.github.com>
2026-07-08 22:09:27 +0530

    Create 125_ConstructBTFromPreorderInorder.cpp

---
+++ Day38/125_ConstructBTFromPreorderInorder.cpp
@@ -0,0 +1,47 @@
+// Problem: Construct Binary Tree from Preorder and Inorder Traversal
+// Link: https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+// Difficulty: Medium
+// Approach: Recursively build the tree using preorder for the root and inorder to split left & right subtrees.
+// Time: O(n) | Space: O(n)
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* solve(vector<int>& preorder, int preStart, int preEnd, vector<int>& inorder, int inStart, int inEnd, map<int, int>& mp){
+        if(preStart > preEnd || inStart > inEnd) return NULL;
+
+        TreeNode* root = new TreeNode(preorder[preStart]);
+
+        int inRoot = mp[root->val];
+        int numsLeft = inRoot - inStart;
+
+        root->left = solve(preorder, preStart+1, preStart + numsLeft, inorder, inStart, inRoot - 1, mp);
+        root->right = solve(preorder, preStart + numsLeft + 1, preEnd, inorder, inRoot + 1, inEnd, mp);
+
+        return root;
+    }
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        map<int, int>mp;
+        int n = preorder.size();
+
+        for(int i=0; i<n; i++){
+            mp[inorder[i]] = i;
+        } 
+
+        TreeNode* root = solve(preorder, 0, n-1, inorder, 0, n-1, mp);
+        return root;
+    }
+};
