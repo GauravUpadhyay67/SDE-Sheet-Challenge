ecf1512a58f3aac992dd33b41cc13de928bf2772
Gaurav Upadhyay <148449614+GauravUpadhyay67@users.noreply.github.com>
2026-07-09 21:58:48 +0530

    Update 130_ChildrenSumProperty.cpp

--- Day39/130_ChildrenSumProperty.cpp
+++ Day39/130_ChildrenSumProperty.cpp
@@ -3,3 +3,42 @@
 // Difficulty: Medium
 // Approach: DFS — each node value = sum of children values
 // Time: O(n) | Space: O(h)
+
+
+
+/*
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+*/
+
+class Solution {
+  public:
+    int isSumProperty(Node *root) {
+        if(!root) return 1;
+        if(!root->left && !root->right) return 1;
+        
+        int leftAns = isSumProperty(root->left);
+        int rightAns = isSumProperty(root->right);
+        
+        int sum = 0;
+        if(root->left) sum += root->left->data;
+        if(root->right) sum += root->right->data;
+        
+        if(sum != root->data) return 0;
+        
+        return leftAns && rightAns;
+        
+    }
+};
