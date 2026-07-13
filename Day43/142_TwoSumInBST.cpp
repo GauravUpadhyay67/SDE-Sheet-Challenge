2732aa7cd9ca418dac60df9d9500d5a0704d1bc3
Gaurav Upadhyay <148449614+GauravUpadhyay67@users.noreply.github.com>
2026-07-13 21:16:04 +0530

    Create 142_TwoSumInBST.cpp

---
+++ Day43/142_TwoSumInBST.cpp
@@ -0,0 +1,46 @@
+// Problem: Two Sum in BST
+// Link: https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
+// Difficulty: Easy
+// Approach: Inorder Traversal + Two Pointer
+// Time: O(n) | Space: O(n)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right)
+ *         : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+    void inorder(TreeNode* root, vector<int>& nums){
+        if(!root) return;
+
+        inorder(root->left, nums);
+        nums.push_back(root->val);
+        inorder(root->right, nums);
+    }
+
+public:
+    bool findTarget(TreeNode* root, int k) {
+        vector<int> nums;
+        inorder(root, nums);
+
+        int l = 0, r = nums.size() - 1;
+
+        while(l < r){
+            int sum = nums[l] + nums[r];
+
+            if(sum == k) return true;
+            else if(sum < k) l++;
+            else r--;
+        }
+      
+        return false;
+    }
+};
